a589f93a0cd739b7c2ffbb4baf54737172063554
Manishkip <89375969+Manishkip@users.noreply.github.com>
2022-02-06 21:59:20 +0530

    Add files via upload

---
+++ Sdata.jsx
@@ -0,0 +1,52 @@
+const Sdata=[
+    {
+        id:1,
+        imgsrc:"https://occ-0-4995-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUOA_El0kPg9x08w7xvCUL3Dg7G-aCIyrJVbY8h6KOj1YjwwcwIBCJ1egByPlFTXgf4tu2rcxAGx95EFDY0zHLGee-LaKtVB3hN2rychtAkIL-oIZJrA8KN1eBbA0Q.jpg?r=647",
+        sname:"DARK",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        id:2,
+        imgsrc:"https://occ-0-4995-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABefoASo9Wcqk5vr4GCLHapgzGhRzcX4rkcTvfGPTw4hYopx9EoWL7kahu3KApHdVGJnpQYHzkccf3qi3tb7KrzzMpdt85YRQZtGr5mfSDQPtKlz8eD_6RZUM_dOlbg.jpg?r=174",
+        sname:"STRANGER",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80187302"
+    },
+    {
+        id:3,
+        imgsrc:"https://occ-0-4995-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABT9f0_XVLifOhBoY5a-elSK20Q3rHPqbozsqH5YVItWrCLcSuOSTrj-RPpK21zzvlR-lUtKO6txbqYuXym5F7nvM2bnP.jpg?r=7a8",
+        sname:"THE GOOD DETECTIVE",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/81323914"
+    },
+    {
+        id:4,
+        imgsrc:"https://occ-0-4995-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUOA_El0kPg9x08w7xvCUL3Dg7G-aCIyrJVbY8h6KOj1YjwwcwIBCJ1egByPlFTXgf4tu2rcxAGx95EFDY0zHLGee-LaKtVB3hN2rychtAkIL-oIZJrA8KN1eBbA0Q.jpg?r=647",
+        sname:"EXTRA CURRICULAR",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80990668"
+    },
+    {
+        id:5,
+        imgsrc:"https://occ-0-4995-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABSsi0T01zpSqIkO1fsn_hsZovFngsEG-HVCYwah5nIL3TFESkZWvuD9VBP_o0CwuLETC0OQYQBt6yIxtUnOWyT4G5CC6D5eN9W7_5QORSNCSQpVw7c29tFamK6NwTw.jpg?r=03c",
+        sname:"VAGABOUND",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/81095101"
+    },
+    {
+        id:6,
+        imgsrc:"https://occ-0-4995-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABaHtSWE0wPaDBgrgxkPxvhQ0QDJAjDQHHrGd5bAdN-OzaY8FVakt_xVK3mvQJK8S7sPp88zIsPythDKy9mFEZHcMv_2d.jpg?r=cd2",
+        sname:"FLOWER OF EVIL",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/81357268"
+    },
+    {
+        id:7,
+        imgsrc:"https://occ-0-4995-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABSsi0T01zpSqIkO1fsn_hsZovFngsEG-HVCYwah5nIL3TFESkZWvuD9VBP_o0CwuLETC0OQYQBt6yIxtUnOWyT4G5CC6D5eN9W7_5QORSNCSQpVw7c29tFamK6NwTw.jpg?r=03c",
+        sname:"VAGABOUND",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/81095101"
+    }
+];
+export default Sdata;
